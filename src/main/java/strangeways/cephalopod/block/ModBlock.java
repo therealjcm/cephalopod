dc41c1c99baebea26df251a229ce73f3b104955e
Jason Messer <jasoncmesser@gmail.com>
2016-09-29 14:59:54 -0700

    update mc mappings

--- src/main/java/strangeways/cephalopod/block/ModBlock.java
+++ src/main/java/strangeways/cephalopod/block/ModBlock.java
@@ -2,6 +2,12 @@ package strangeways.cephalopod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import strangeways.cephalopod.MainMod;
 
 /**
@@ -14,5 +20,20 @@ public class ModBlock extends Block {
         setUnlocalizedName(name);
         setCreativeTab(MainMod.tabTutorial);
         setHardness(10f);
+    }
+
+    @Override
+    public boolean addDestroyEffects(World world, BlockPos pos, ParticleManager manager)
+    {
+        TextComponentString msg = new TextComponentString(TextFormatting.DARK_GREEN +
+                "addDestroyEffects");
+        if (world.isRemote)
+        {
+            msg.appendText("-remote");
+        }
+        world.playerEntities.stream().forEach((player) -> player.addChatComponentMessage(msg));
+        // EntityPlayer player = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 10f, true);
+        // player.addChatComponentMessage(msg);
+        return super.addDestroyEffects(world, pos, manager);
     }
 }
